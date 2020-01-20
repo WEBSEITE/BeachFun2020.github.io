9da2cea2b18c8e94cc27160ce1e0b353f481aa50
lancfreeman <58507934+lancfreeman@users.noreply.github.com>
2020-01-20 09:33:41 +0100

    Start

---
+++ js/java.js
@@ -0,0 +1,26 @@
+var ReleaseDate = new Date("Mar 7, 2020 17:00:00").getTime();
+var TimerFunction = setInterval(function() {
+    var DatumHeute = new Date().getTime();
+    var Differenz = ReleaseDate - DatumHeute;
+
+    var d = Math.floor(Differenz / (1000 * 60 * 60 * 24));
+    var h = Math.floor((Differenz % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    var m = Math.floor((Differenz % (1000 * 60 * 60)) / (1000 * 60));
+    var s = Math.floor((Differenz % (1000 * 60)) / 1000);
+
+    document.getElementById("timer").innerHTML =
+        "<span>" +
+        d +
+        "<br><br><i>Tage</i></span><span>" +
+        h +
+        "<br><br><i>Stunden</i></span><span>" +
+        m +
+        "<br><br><i>Minuten</i></span><span>" +
+        s +
+        "<br><br><i>Sekunden</i></span>";
+
+    if (Differenz < 0) {
+        clearInterval(TimerFunction);
+        document.getElementById("timer").innerHTML = "Abgelaufen";
+    }
+}, 1000);
